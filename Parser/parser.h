4347e2de1557448bdc5072c3109c58613babd635
warleon <anthony.aguilar@utec.edu.pe>
2020-11-18 12:35:43 -0500

    add rapidjson parser

--- Parser/parser.h
+++ Parser/parser.h
@@ -1,4 +1,8 @@
 #ifndef GRAPH_PROJECT_PANCONPALTA_PARSER_H
 #define GRAPH_PROJECT_PANCONPALTA_PARSER_H
 
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/writer.h"
+#include "include/rapidjson/stringbuffer.h"
+
 #endif //GRAPH_PROJECT_PANCONPALTA_PARSER_H
