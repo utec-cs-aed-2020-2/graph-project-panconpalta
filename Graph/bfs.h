6b0533a914dc69d1884260cfecb607197282cfa4
lordjuacs <juacsr@gmail.com>
2020-12-01 10:06:05 -0500

    add bfs and dfs class

---
+++ Graph/bfs.h
@@ -0,0 +1,10 @@
+#ifndef GRAPH_PROJECT_PANCONPALTA_BFS_H
+#define GRAPH_PROJECT_PANCONPALTA_BFS_H
+
+template<typename data_t, typename weight_t>
+class BFS {
+
+};
+
+
+#endif //GRAPH_PROJECT_PANCONPALTA_BFS_H
---
+++ Graph/dfs.h
@@ -0,0 +1,10 @@
+#ifndef GRAPH_PROJECT_PANCONPALTA_DFS_H
+#define GRAPH_PROJECT_PANCONPALTA_DFS_H
+
+template<typename data_t, typename weight_t>
+class DFS {
+
+};
+
+
+#endif //GRAPH_PROJECT_PANCONPALTA_DFS_H
