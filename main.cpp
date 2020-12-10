da943e511db75bfa1eccd80893a2c5c7d95d4acf
lordjuacs <juacsr@gmail.com>
2020-12-10 11:47:37 -0500

    successful test - Strongly Connected Components

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@
 #include "Graph/dfs.h"
 #include "Graph/FloydWarshall.h"
 #include "Graph/BellmanFord.h"
+#include "Graph/SCC.h"
 #include "Tester/tester.h"
 
 int main() {
     //Tester::executeParser(file::airports);
     //Tester::executeExamples();
     DirectedGraph<int, int> graph;
-    graph.insertVertex("A", 1);
-    graph.insertVertex("B", 2);
-    graph.insertVertex("C", 3);
-    graph.insertVertex("D", 4);
-    graph.insertVertex("E", 5);
-    graph.createEdge("A", "B", 1);
-    graph.createEdge("C", "A", 2);
-    graph.createEdge("C", "B", 6);
-    graph.createEdge("C", "E", 5);
-    graph.createEdge("B", "E", 4);
-    graph.createEdge("E", "D", 3);
+    graph.insertVertex("a", 0);
+    graph.insertVertex("b", 1);
+    graph.insertVertex("c", 2);
+    graph.insertVertex("d", 3);
+    graph.insertVertex("e", 4);
+    graph.insertVertex("f", 4);
+    graph.insertVertex("g", 4);
+    graph.insertVertex("h", 4);
+    graph.insertVertex("i", 4);
+    graph.createEdge("a", "b", 1);
+    graph.createEdge("b", "e", 2);
+    graph.createEdge("e", "a", 6);
+    graph.createEdge("e", "f", 5);
+    graph.createEdge("b", "f", 4);
+    graph.createEdge("f", "g", 4);
+    graph.createEdge("g", "f", 4);
+    graph.createEdge("b", "c", 4);
+    graph.createEdge("c", "d", 4);
+    graph.createEdge("d", "c", 4);
+    graph.createEdge("c", "g", 4);
+    graph.createEdge("d", "h", 4);
+    graph.createEdge("h", "d", 4);
+    graph.createEdge("d", "i", 4);
+    graph.createEdge("i", "d", 4);
     graph.display();
     std::cout << "\n";
-    auto bellman = BellmanFord<int, int>(graph, "A");
-    bellman.displayDistances();
+    auto scc = SCC<int, int>(graph);
+    scc.apply().display();
+    scc.displayComponents();
     return EXIT_SUCCESS;
 }
