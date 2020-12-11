052eb10e14c893f1a2e7613c20b73f7e2224bfe6
lordjuacs <juacsr@gmail.com>
2020-12-11 12:37:30 -0500

    add tester with search algorithms

--- main.cpp
+++ main.cpp
@@ -10,27 +10,7 @@
 
 int main() {
     //Tester::executeParser(file::airports);
-    //Tester::executeExamples();
-    DirectedGraph<int, int> graph;
-    graph.insertVertex("A", 0);
-    graph.insertVertex("B", 0);
-    graph.insertVertex("C", 0);
-    graph.insertVertex("D", 0);
-    graph.insertVertex("E", 0);
-    graph.createEdge("A", "B", 6);
-    graph.createEdge("A", "D", 1);
-    graph.createEdge("B", "D", 2);
-    graph.createEdge("D", "E", 1);
-    graph.createEdge("E", "B", 2);
-    graph.createEdge("C", "B", 5);
-    graph.createEdge("E", "C", 5);
-    graph.display();
-    std::cout << "\n";
-    auto dijkstra = Dijkstra<int, int>(graph, "A");
-    dijkstra.displayDistances();
-    dijkstra.showPath("B");
-    auto bellman = BellmanFord<int, int>(graph, "A");
-    bellman.displayDistances();
-    bellman.showPath("B");
+    Tester::executeExamples();
+
     return EXIT_SUCCESS;
 }
