ce3f43dcb0cf7acbbf21d54274055275e2f2f2f3
Joaquin Ramirez <50754596+lordjuacs@users.noreply.github.com>
2020-12-14 18:42:34 -0500

    Update UndirectedGraph.h

--- Graph/UndirectedGraph.h
+++ Graph/UndirectedGraph.h
@@ -104,7 +104,7 @@ bool undiGraph_t::createEdge(id_t id1, id_t id2, weight_t w) {
     auto edges = this->vertexes[id1]->edges;
     for (auto it = edges.begin(); it != edges.end(); ++it) {
         //si la arista ya existe
-        if ((*it)->vertexes[0].id == id2 || (*it)->vertexes[1].id == id2)
+        if ((*it)->vertexes[0].id == id1 || (*it)->vertexes[1].id == id2)
             return false;
     }
     vertex_t *v = new vertex_t[2];
